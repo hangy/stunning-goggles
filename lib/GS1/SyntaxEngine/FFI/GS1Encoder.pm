a25f54adadcd7179958cc0ec910ad780c8008fcc
hangy <hangy@hangy.de>
2023-08-25 18:15:07 +0000

    fix: Explicitly manage memory
    To avoid weird SEGFAULTs

--- lib/GS1/SyntaxEngine/FFI/GS1Encoder.pm
+++ lib/GS1/SyntaxEngine/FFI/GS1Encoder.pm
@@ -1,191 +1,166 @@
 # SPDX-License-Identifier: GPL-1.0-or-later OR Artistic-1.0-Perl
 
-# ABSTRACT: Provides a FFI wrapper for libgs1encoders
-
 package GS1::SyntaxEngine::FFI::GS1Encoder;
 
-use utf8;
+# ABSTRACT: Provides a FFI wrapper for libgs1encoders
 
-use Moo;
+use utf8;
 
 use strictures 2;
 use namespace::clean;
 
 use FFI::Platypus 2.08;
+use FFI::Platypus::Memory ();
 
-use Alien::libgs1encoders 0.02;
+use Alien::libgs1encoders 0.03;
 
-use GS1::SyntaxEngine::FFI::InitException;
-use GS1::SyntaxEngine::FFI::EncoderParameterException;
-
-my $opaque_types = [map {"gs1_$_"} qw/encoder/];
-
-my $functions = [
-  [getVersion => ['void'] => 'string'],
-  [init => ['void'] => 'gs1_encoder'],
-  [getErrMsg => ['gs1_encoder'] => 'string'],
-  [getSym => ['gs1_encoder'] => 'int'],
-  [setSym => ['gs1_encoder', 'int'] => 'bool'],
-  [getAddCheckDigit => ['gs1_encoder'] => 'bool'],
-  [setAddCheckDigit => ['gs1_encoder', 'bool'] => 'bool'],
-  [getPermitUnknownAIs => ['gs1_encoder'] => 'bool'],
-  [setPermitUnknownAIs => ['gs1_encoder', 'bool'] => 'bool'],
-  [getPermitZeroSuppressedGTINinDLuris => ['gs1_encoder'] => 'bool'],
-  [setPermitZeroSuppressedGTINinDLuris => ['gs1_encoder', 'bool'] => 'bool'],
-  [getIncludeDataTitlesInHRI => ['gs1_encoder'] => 'bool'],
-  [setIncludeDataTitlesInHRI => ['gs1_encoder', 'bool'] => 'bool'],
-  [getValidateAIassociations => ['gs1_encoder'] => 'bool'],
-  [setValidateAIassociations => ['gs1_encoder', 'bool'] => 'bool'],
-  [getDataStr => ['gs1_encoder'] => 'string'],
-  [setDataStr => ['gs1_encoder', 'string'] => 'bool'],
-  [getAIdataStr => ['gs1_encoder'] => 'string'],
-  [setAIdataStr => ['gs1_encoder', 'string'] => 'bool'],
-  [getDLuri => ['gs1_encoder','string'] => 'string'],
-  [getScanData => ['gs1_encoder'] => 'string'],
-  [setScanData => ['gs1_encoder', 'string'] => 'bool'],
-  [free => ['gs1_encoder'] => 'void']
-];
-
-my $ffi = FFI::Platypus->new( lib => [ Alien::libgs1encoders->dynamic_libs ] );
-
-for my $type (@{ $opaque_types }) {
-  $ffi->custom_type($type => {
-    native_type => 'opaque',
-    native_to_perl => sub {
-      my $class = "GS1::SyntaxEngine::FFI::Types::$type";
-      bless \$_[0], $class;
-    },
-    perl_to_native => sub {
-      my $val = shift;
-      die "Wrong type passed, ".ref($val)." expected GS1::SyntaxEngine::FFI::Types::$type" unless ref($val) eq "GS1::SyntaxEngine::FFI::Types::$type";
-
-      return ${ $val }
-  }});
-
-  $ffi->type('opaque' => $type . '_ptr');
-}
-
-for my $function (@{ $functions }) {
-  my $name = shift @{ $function };
-  $ffi->attach(["gs1_encoder_$name" => "_$name"], @{ $function });
-}
-
-has _encoder => (
-  is => 'rw',
+my $ffi = FFI::Platypus->new(
+    api => 2,
+    lib => [ Alien::libgs1encoders->dynamic_libs ]
+);
+$ffi->mangler(
+    sub {
+        my ($name) = @_;
+        "gs1_encoder$name";
+    }
 );
 
+$ffi->attach( _instanceSize        => ['void']                   => 'size_t' );
+$ffi->attach( _init                => ['opaque']                 => 'int' );
+$ffi->attach( _getVersion          => ['opaque']                 => 'string' );
+$ffi->attach( _getErrMsg           => ['opaque']                 => 'string' );
+$ffi->attach( _getSym              => ['opaque']                 => 'int' );
+$ffi->attach( _setSym              => [ 'opaque', 'int' ]        => 'bool' );
+$ffi->attach( _getAddCheckDigit    => ['opaque']                 => 'bool' );
+$ffi->attach( _setAddCheckDigit    => [ 'opaque', 'bool' ]       => 'bool' );
+$ffi->attach( _getPermitUnknownAIs => ['opaque']                 => 'bool' );
+$ffi->attach( _setPermitUnknownAIs => [ 'opaque', 'bool' ]       => 'bool' );
+$ffi->attach( _getPermitZeroSuppressedGTINinDLuris => ['opaque'] => 'bool' );
+$ffi->attach(
+    _setPermitZeroSuppressedGTINinDLuris => [ 'opaque', 'bool' ] => 'bool' );
+$ffi->attach( _getIncludeDataTitlesInHRI => ['opaque']           => 'bool' );
+$ffi->attach( _setIncludeDataTitlesInHRI => [ 'opaque', 'bool' ] => 'bool' );
+$ffi->attach( _getValidateAIassociations => ['opaque']           => 'bool' );
+$ffi->attach( _setValidateAIassociations => [ 'opaque', 'bool' ] => 'bool' );
+$ffi->attach( _getDLuri     => [ 'opaque', 'string' ]            => 'string' );
+$ffi->attach( _getScanData  => ['opaque']                        => 'string' );
+$ffi->attach( _setScanData  => [ 'opaque', 'string' ]            => 'bool' );
+$ffi->attach( _getDataStr   => ['opaque']                        => 'string' );
+$ffi->attach( _setDataStr   => [ 'opaque', 'string' ]            => 'bool' );
+$ffi->attach( _getAIdataStr => ['opaque']                        => 'string' );
+$ffi->attach( _setAIdataStr => [ 'opaque', 'string' ]            => 'bool' );
+
+sub new {
+    my ($class) = @_;
+    my $size    = _instanceSize();
+    my $self    = bless \FFI::Platypus::Memory::malloc($size), $class;
+    my $r       = _init( ${$self} );
+    return $self;
+}
+
 sub _throw_error_exception {
-  my ($self) = @_;
-  GS1::SyntaxEngine::FFI::EncoderParameterException->throw({ message => $self->error_msg });
-  return;
+    my ($self) = @_;
+    GS1::SyntaxEngine::FFI::EncoderParameterException->throw(
+        { message => $self->error_msg } );
+    return;
 }
 
 sub ai_data_str {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setAIdataStr($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setAIdataStr( ${$self}, $value ) or $self->_throw_error_exception();
+    }
 
-  return _getAIdataStr($self->_encoder);
+    return _getAIdataStr( ${$self} );
 }
 
 sub data_str {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setDataStr($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setDataStr( ${$self}, $value ) or $self->_throw_error_exception();
+    }
 
-  return _getDataStr($self->_encoder);
+    return _getDataStr( ${$self} );
 }
 
 sub scan_data {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setScanData($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setScanData( ${$self}, $value ) or $self->_throw_error_exception();
+    }
 
-  return _getScanData($self->_encoder);
+    return _getScanData( ${$self} );
 }
 
 sub error_msg {
-  my ($self) = @_;
-  return _getErrMsg($self->_encoder);
+    my ($self) = @_;
+    return _getErrMsg( ${$self} );
 }
 
 sub add_check_digit {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setAddCheckDigit($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setAddCheckDigit( ${$self}, $value )
+          or $self->_throw_error_exception();
+    }
 
-  return _getAddCheckDigit($self->_encoder);
+    return _getAddCheckDigit( ${$self} );
 }
 
 sub permit_unknown_ais {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setPermitUnknownAIs($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setPermitUnknownAIs( ${$self}, $value )
+          or $self->_throw_error_exception();
+    }
 
-  return _getPermitUnknownAIs($self->_encoder);
+    return _getPermitUnknownAIs( ${$self} );
 }
 
 sub permit_zero_suppressed_gtin_in_dl_uris {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setPermitZeroSuppressedGTINinDLuris($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setPermitZeroSuppressedGTINinDLuris( ${$self}, $value )
+          or $self->_throw_error_exception();
+    }
 
-  return _getPermitZeroSuppressedGTINinDLuris($self->_encoder);
+    return _getPermitZeroSuppressedGTINinDLuris( ${$self} );
 }
 
 sub include_data_titles_in_hri {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setIncludeDataTitlesInHRI($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setIncludeDataTitlesInHRI( ${$self}, $value )
+          or $self->_throw_error_exception();
+    }
 
-  return _getIncludeDataTitlesInHRI($self->_encoder);
+    return _getIncludeDataTitlesInHRI( ${$self} );
 }
 
 sub validate_ai_associations {
-  my ($self, $value) = @_;
-  if (@_ == 2) {
-    _setValidateAIassociations($self->_encoder, $value) or $self->_throw_error_exception();
-  }
+    my ( $self, $value ) = @_;
+    if ( @_ == 2 ) {
+        _setValidateAIassociations( ${$self}, $value )
+          or $self->_throw_error_exception();
+    }
 
-  return _getValidateAIassociations($self->_encoder);
+    return _getValidateAIassociations( ${$self} );
 }
 
 sub version {
-  my ($self) = @_;
-  return _getVersion($self->_encoder);
+    my ($self) = @_;
+    return _getVersion( ${$self} );
 }
 
 sub dl_uri {
-  my ($self, $domain) = @_;
-  return _getDLuri($self->_encoder, $domain);
+    my ( $self, $domain ) = @_;
+    return _getDLuri( ${$self}, $domain );
 }
 
-sub BUILD {
-  my ($self, $args) = @_;
-  my $encoder = _init();
-  if (!$encoder) {
-    GS1::SyntaxEngine::FFI::InitException->throw();
-  }
-
-  $self->_encoder($encoder);
-  return $self;
-}
-
-sub DEMOLISH {
-  my ($self, $in_global_destruction) = @_;
-  my $encoder = $self->_encoder;
-  if ($encoder) {
-    _free($encoder);
-  }
-
-  return $self;
+sub DESTROY {
+    my ($self) = @_;
+    FFI::Platypus::Memory::free( ${$self} );
+    return $self;
 }
 
 1;
